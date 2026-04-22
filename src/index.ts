21bbc29a2917835a8d8593c5705aa776aa24d2d5
Francisco Victoriano <fvictoriano.dev@gmail.com>
2026-04-22 10:19:04 -0400

    [feat] tcp connection on port 21

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import { DEFAULT_CONNECTION_PORT } from "./config";
+
+// DOCS: https://bun.com/docs/runtime/networking/tcp
+Bun.listen({
+  hostname: "localhost",
+  port: DEFAULT_CONNECTION_PORT,
+  socket: {
+    open(socket) {},
+    close(socket) {},
+    error(socket) {},
+    data(socket, data) {},
+  },
+  //SFTP beyond scope.
+  tls: false,
+});
